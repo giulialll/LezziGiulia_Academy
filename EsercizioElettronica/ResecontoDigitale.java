8a02659347b3f0999870234156709e318abb7a29
giulialll <lezzi1giulia@gmail.com>
2024-03-14 12:49:43 +0100

    sistemato pezzo del main

--- EsercizioElettronica/ResecontoDigitale.java
+++ EsercizioElettronica/ResecontoDigitale.java
@@ -8,41 +8,41 @@ public class ResecontoDigitale {
         Scanner myScan = new Scanner(System.in);
         System.out.println("Inserire il nome dell'app che vuoi far partire");
         String nomeApp = myScan.nextLine();
-        //dispositivo elettronico generale 
+        // dispositivo elettronico generale
         DispositivoElettronico dispositivoElettronico = new DispositivoElettronico(nomeApp);
         dispositivoElettronico.avviaApplicazione(nomeApp);
         // smartphone
         System.out.println("Smartphone:");
         String username = "giulia";
         String password = "pass";
-        Smartphone smartphone = new Smartphone(nomeApp,username,password);
+        Smartphone smartphone = new Smartphone(nomeApp, username, password);
         System.out.println("Inserire le credenziali");
         String usernameUtente = myScan.nextLine();
         String passwordUtente = myScan.nextLine();
-        if(smartphone.login(usernameUtente, passwordUtente)){
+        if (smartphone.login(usernameUtente, passwordUtente)) {
             smartphone.avviaApplicazione(nomeApp);
-        }else{
+            System.out.println("Vuoi vedere le tue credenziali?S/N");
+            String rispostaCredenziali = myScan.nextLine();
+            if (rispostaCredenziali.equalsIgnoreCase("S")) {
+                System.out.println(smartphone.getUsername());
+                System.out.println(smartphone.getPassword());
+            }
+        } else {
             System.out.println("credenziali errate");
         }
-        System.out.println("Vuoi vedere le tue credenziali?S/N");
-        String rispostaCredenziali = myScan.nextLine();
-        if(rispostaCredenziali.equalsIgnoreCase("S")){
-            System.out.println(smartphone.getUsername());
-            System.out.println(smartphone.getPassword()); 
-        }
-        //tablet
+
+        // tablet
         System.out.println("Tablet:");
         Tablet tablet = new Tablet(nomeApp);
         tablet.avviaApplicazione(nomeApp);
         System.out.println("Vuoi vedere i tuoi punti e il tuo id?S/N");
         String rispostaTablet = myScan.nextLine();
-        if(rispostaTablet.equalsIgnoreCase("S")){
+        if (rispostaTablet.equalsIgnoreCase("S")) {
             System.out.println(tablet.getIdDaIncrementare());
-            System.out.println(tablet.getPunti()); 
+            System.out.println(tablet.getPunti());
         }
         myScan.close();
         System.out.println("Fine applicazione");
 
-        
     }
 }
